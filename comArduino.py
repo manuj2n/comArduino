485b67943ce3c009b960c8985c7d7b5daa9f4651
manuj2n <manu.j2n@gmail.com>
2018-05-27 17:00:25 +0000

    traitement date heure

--- comArduino.py
+++ comArduino.py
@@ -3,24 +3,34 @@
 #
 # comArduino.py
 
-
+import time
+import datetime
 import serial
 
-fichier='/home/pi/agenda.txt'
+heureActuel = datetime.time(0, 0, 0)
+heureVeilleProchaine = datetime.time(0, 0 ,0)
+heureTmp = datetime.time(0, 0, 0)
+fichier='agenda.txt'
 listVeille=[]
+
+def StringToHeure(chaineHeure):
+	print(chaineHeure)
 
 def OuvreAgenda():
 	f = open(fichier, 'r')
 	while True :
 		chaine = f.readline()
-		print(chaine)
+		listVeille.append(chaine)
 		if ("" == chaine):
 			print("fin de fichier")
 			f.close()
 			break
+		print(chaine)
 
 OuvreAgenda()
 ser = serial.Serial('/dev/ttyS0', 9600, dsrdtr=0)
 line = 'bonjour\n'
 ser.write(line)
 ser.close()
+print(listVeille)
+
