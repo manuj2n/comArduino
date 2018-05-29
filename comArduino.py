fd7b47a80fea411ba592d23689d39d237fbf061f
manuj2n <manu.j2n@gmail.com>
2018-05-29 13:13:50 +0000

    conversion string to date heure

--- comArduino.py
+++ comArduino.py
@@ -11,27 +11,35 @@ heureActuel = datetime.time(0, 0, 0)
 heureVeilleProchaine = datetime.time(0, 0 ,0)
 heureTmp = datetime.time(0, 0, 0)
 fichier='agenda.txt'
-listVeille=[]
-
-def StringToHeure(chaineHeure):
-	print(chaineHeure)
 
 def OuvreAgenda():
 	f = open(fichier, 'r')
 	while True :
 		chaine = f.readline()
-		listVeille.append(chaine)
-		if ("" == chaine):
-			print("fin de fichier")
-			f.close()
-			break
 		print(chaine)
+		if ("" == chaine):
+                        print("fin de fichier")
+                        f.close()
+                        break
+		if (chaine[0] == "V"):
+			print(chaine[1:5])
+			heureVeilleProchaine = time.strptime(chaine[1:5], '%H%M')
+			print(heureVeilleProchaine.tm_hour, heureVeilleProchaine.tm_min)
 
 
+#heureActuel = datetime.time.now()
+print(heureActuel)
 OuvreAgenda()
+# chercher heureProchaineVeille
+# 
+# si heureCourante < heureProchaineVeille
+# 	ne rien faire
+# si heureCourante > heureProchaineVeille
+#	transmettre duree de veille
+#	shutdown
+#
 ser = serial.Serial('/dev/ttyS0', 9600, dsrdtr=0)
 line = 'bonjour\n'
 ser.write(line)
 ser.close()
-print(listVeille)
 
