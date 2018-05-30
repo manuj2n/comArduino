4df3027097e2a51cea067e8b7b1b1e790504442d
manuj2n <manu.j2n@gmail.com>
2018-05-30 22:26:01 +0200

    code fonctionne, mais pas fini

--- comArduino.py
+++ comArduino.py
@@ -7,14 +7,15 @@ import datetime
 import time
 #import serial
 
-hT = datetime.time(0,0,0)
-hPV = datetime.time(0,0,0)
+hT = datetime.time(0, 0, 0)
+hPV = datetime.time(0, 0, 0)
 dureeVeille = 0
-fichier='agenda.txt'
+fichier = 'agenda.txt'
+
 
 def OuvreAgenda():
 	f = open(fichier, 'r')
-	while True :
+	while True:
 		chaine = f.readline()
 		#print(chaine)
 		if (chaine == ""):
@@ -27,26 +28,32 @@ def OuvreAgenda():
 			heureDureeVeille = chaine[7:9]
 			minDureeVeille = chaine[10:12]
 			dureeVeille = (int(heureDureeVeille) * 3600) + (int(minDureeVeille) * 60)
-			print("hDV et mDV en secondes", heureDureeVeille, minDureeVeille,dureeVeille) 
+			print("hDV et mDV en secondes", heureDureeVeille, minDureeVeille, dureeVeille)
 			hT = datetime.datetime(hC.year, hC.month, hC.day, int(heure), int(min))
-			print("heure Tempo",hT)
-			print("Temps d'attente pour ma veille",hT - hC)
+			print("heure Tempo", hT)
+			print("Temps d'attente pour ma veille", hT - hC)
 			if (hT > hC):
 				f.close()
 				return hT
 
 hC = datetime.datetime.now()
-print("heure Courante",hC)
+print("heure Courante", hC)
 hPV = OuvreAgenda()
-print("heure Prochaine Veille",hPV)
+if (hPV == None):
+	time.sleep(5)
+	# a voir, plus d'heure de veille avant le lendemain
+print("heure Prochaine Veille", hPV)
 while True:
-    hC = datetime.datetime.now()
-    if hC < hPV :
-		print("heure courant",hC)
-		print("heure de veille",hPV)
-	else:
-    	print("transmission vers l'arduino")
-		# si heureCourante > heureProchaineVeille
+	hC = datetime.datetime.now()
+	if hC < hPV:
+		print("heure courant", hC)
+		print("heure de veille", hPV)
+		time.sleep(5)
+	else:  		
+		time.sleep(5)
+		print("transmission vers l'arduino")
+		# si heureCourante > heure
+		# ProchaineVeille
 		#	transmettre duree de veille
 		#	shutdown
 		#
